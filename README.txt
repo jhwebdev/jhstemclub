6d4dc8e7708fa8b100fa2db1a5d48fc38cd5377b
Mr. C <38505354+jhwebdev@users.noreply.github.com>
2018-05-14 10:26:57 -0700

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,37 @@
+address: jhstemclub
+
+
+TO DO:
+-fix weird white column in velvet style (something with grids)
+	-column got wider:(
+-add picture gallery (modals?)
+	-array for images
+-adjust style and music buttons
+-fix embedded minigame
+
+27-02-18 "home" is the latest iteration.
+
+28-02-18 -fixed mascot and links
+	 -fixed columns and spacing
+	 -added link hover highlights 
+
+05-03-18 -adding grid stlying to mainstyle.css
+	 -fixed separate mobile stylesheet
+13-03-18 -added header background
+	 -multiple 'p' not stacking
+13-04-18 -added svg image for robot mascot
+	 -tried to do hiding menu
+	 -didn't realize but file links and css links are broken
+23-04-18 -adding an alternate style when a button is pressed (javascript)
+24-04-18 -converted all images to svg
+	 -fixed velvet style colours
+	 -updated template html
+27-04-18 -added music
+	 -attempting to switch music on style change
+	 -added 'images' page
+01-05-18 -added 'minigame' page
+	 -fixed mobile style
+02-05-18 -removed extra grid parent on maintexts (mainbody removed) on all pages
+	 -tweaked 'STEM' title colour
+	 -embedded minigame
+	 -image folder
---
+++ scriptFile.js
@@ -0,0 +1,59 @@
+//alert('Test! Yoyoyo!');
+
+var myImage = document.querySelector('#sidePhoto');
+var myCSS = document.querySelector('stylesheet');
+var myMCSS = document.querySelector('mobilestyle');
+
+function swapStyle(sheet)
+{
+	document.getElementById('mainstyle').setAttribute('href', sheet);
+}
+
+//tried reacreating swapStyle function but for music
+function swapMusic(music, type)
+{
+	document.getElementById('songs').setAttribute('src', music);
+	//document.getElementById('songs').setAttribute('type', type);
+}
+
+var audio = document.getElementById('Songs');
+
+function styles() 
+{
+	var style1 = document.getElementById('stylish1');
+	var style2 = document.getElementById('stylish2');
+	var music1 = document.getElementById('music1');
+	var music2 = document.getElementById('music2');
+	
+	stylish1.onclick = 
+		function() {
+			
+			swapStyle('mainstyle.css');
+			swapMusic('whenTheMoonsReachingStars.mp3');
+			//swapMobile('mobilestyle.css');
+		}
+	stylish2.onclick = 
+		function() {
+			
+			swapStyle('velvetMainStyle.css');
+			swapMusic('poemForEveryonesSouls.mp3');
+			//swapMobile('velvetMobileStyle.css');
+		}
+	/*music1.onclick =
+		function () {
+			swapMusic('whenTheMoonsReachingStars.mp3');
+		}
+	music2.onclick =
+		function () {
+			swapMusic('poemForEveryonesSouls.mp3');
+		}*/
+}
+
+/*var zero: "whenTheMoonsReachingStars.mp3";
+
+function Music()
+{
+	number = new Audio[1];
+	
+	number[0] = one;
+}*/
